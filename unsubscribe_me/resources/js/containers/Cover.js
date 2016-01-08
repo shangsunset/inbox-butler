9eb0f93727fdab60628e673cb0da51983ad9ab38
shangsunset <shangsunset@gmail.com>
2016-01-07 20:25:22 -0500

    fixed typos

--- unsubscribe_me/resources/js/containers/Cover.js
+++ unsubscribe_me/resources/js/containers/Cover.js
@@ -10,7 +10,8 @@ export default class Cover extends Component {
     return (
         <div>
           <h1 className="cover-heading">Say bye to annoying subscriptions</h1>
-          <p className="lead">Unsubscribe Me is born out of frustration to annoying subscriptions. It helps you unsubscribe from various newsletters with just on click :)</p>
+          <p className="lead">Unsubscribe Me is born out of frustration with annoying subscriptions in email inboxes. It helps you unsubscribe from various newsletters with just one click :)</p>
+          <p className="lead">(Currently only support Gmail)</p>
           <p className="lead">
             <a href="/login" className="btn btn-lg btn-default">Get Started!</a>
           </p>
